b89d0f62b437822e02c77ffd0e6929e8ab62f249
ToxicOtter <luan.lima.lopes16@gmail.com>
2021-06-07 19:31:33 -0300

    Remaking attributes section structure

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import './App.scss';
 import React from 'react';
 
-function App(){
+function App() {
   return (
     <form className="container">
       <Header />
-      <StatusLeft />
-      <StatusMid />
-      <StatusRight />
-      <AttaksAndSummons />
-      <CharacteristcsAndTalents />
-      <AnotherProeficiences />
-      <EquipamentsAndNotes />
+      <main>
+        <StatusLeft />
+        <StatusMid />
+        <StatusRight />
+        <AttaksAndSummons />
+        <CharacteristcsAndTalents />
+        <AnotherProeficiences />
+        <EquipamentsAndNotes />
+      </main>
     </form>
   )
 }
 
-function Header(){
+function Header() {
   return (
     <header>
       <section className="charname">
@@ -27,7 +29,7 @@ function Header(){
         <ul>
           <li>
             <label for="classeNivel">Classe & Nível</label>
-            <input type="text" name="classeNivel"></input>    
+            <input type="text" name="classeNivel"></input>
           </li>
           <li>
             <label for="antecedentes">Antecedente</label>
@@ -55,209 +57,255 @@ function Header(){
   )
 }
 
-function StatusLeft(){
+function StatusLeft() {
   return (
-    <div className="status-left">
-      <form>
-        <label>
-          Bônus de Proeficiência
-          <input type="number"></input>
-        </label>
-        <label>
-          Inspiração
-          <input type="text"></input>
-        </label>
+    <section>
+      <section className="atributos">
+        <div className="scores">
+          <ul>
+            <li>
+              <div className="score">
+                <label for="forcaAtributo">Força</label>
+                <input type="number" name="forcaAtributo" placeholder="10"></input>
+              </div>
+            </li>
 
-        <div>
-          <label>
-            Força
-            <input type="number"></input>
-          </label>
-          <fieldset>
-            <label>
-              Salvaguarda
-              <input type="checkbox"></input>
-            </label>
-            <label>
-              Atletismo
-              <input type="checkbox"></input>
-            </label>
-          </fieldset>
+            <li>
+              <div className="score">
+                <label for="destrezaAtributo">Destreza</label>
+                <input type="number" name="destrezaAtributo" placeholder="10"></input>
+              </div>
+            </li>
+
+            <li>
+              <div className="score">
+                <label for="constituicaoAtributo">Constituição</label>
+                <input type="number" name="constituicaoAtributo" placeholder="10"></input>
+              </div>
+            </li>
+
+            <li>
+              <div className="score">
+                <label for="inteligenciaAtributo">Inteligência</label>
+                <input type="number" name="inteligenciaAtributo" placeholder="10"></input>
+              </div>
+            </li>
+
+            <li>
+              <div className="score">
+                <label for="sabedoriaAtributo">Sabedoria</label>
+                <input type="number" name="sabedoriaAtributo" placeholder="10"></input>
+              </div>
+            </li>
+
+            <li>
+              <div className="score">
+                <label for="carismaAtributo">Carisma</label>
+                <input type="number" name="carismaAtributo" placeholder="10"></input>
+              </div>
+            </li>
+          </ul>
         </div>
 
-        <div>
-          <label>
-            Destreza
-            <input type="number"></input>
-          </label>
-          <fieldset>
-            <label>
-              Salvaguarda
-              <input type="checkbox"></input>
-            </label>
-            <label>
-              Acrobacia
-              <input type="checkbox"></input>
-            </label>
-            <label>
-              Furtividade
-              <input type="checkbox"></input>
-            </label>
-            <label>
-              Prestidigitação
-              <input type="checkbox"></input>
-            </label>
-          </fieldset>
+        <div className="attr-aplicacoes">
+          <div class="inspiracao box">
+            <div class="label-container">
+              <label for="inspiracao">Inspiração</label>
+            </div>
+            <input name="inspiracao" type="checkbox" />
+          </div>
+
+          <div class="bonusProeficiencia box">
+            <div class="label-container">
+              <label for="bonusProeficiencia">Bônus de Proeficiência</label>
+            </div>
+            <input name="bonusProeficiencia" placeholder="+2" />
+          </div>
         </div>
 
-        <div>
-          <label>
-            Constituição
-            <input type="number"></input>
-          </label>
-          <fieldset>
-            <label>
-              Salvaguarda
-              <input type="checkbox"></input>
-            </label>
-          </fieldset>
+        <div className="salvaguardas secaoList box">
+          <ul>
+            <li>
+              <label for="forcaSalvaguarda">Força</label>
+              <input name="forcaSalvaguarda" placeholder="+0" type="text" />
+              <input name="forcaSalvaguardaProva" type="checkbox" />
+            </li>
+
+            <li>
+              <label for="destrezaSalvaguarda">Destreza</label>
+              <input name="destrezaSalvaguarda" placeholder="+0" type="text" />
+              <input name="destrezaSalvaguardaProva" type="checkbox" />
+            </li>
+
+            <li>
+              <label for="constituicaoSalvaguarda">Constituicao</label>
+              <input name="constituicaoSalvaguarda" placeholder="+0" type="text" />
+              <input name="constituicaoSalvaguardaProva" type="checkbox" />
+            </li>
+
+            <li>
+              <label for="inteligenciaSalvaguarda">Inteligência</label>
+              <input name="inteligenciaSalvaguarda" placeholder="+0" type="text" />
+              <input name="inteligenciaSalvaguardaProva" type="checkbox" />
+            </li>
+
+            <li>
+              <label for="sabedoriaSalvaguarda">Sabedoria</label>
+              <input name="sabedoriaSalvaguarda" placeholder="+0" type="text" />
+              <input name="sabedoriaSalvaguardaProva" type="checkbox" />
+            </li>
+
+            <li>
+              <label for="carismaSalvaguarda">Carisma</label>
+              <input name="carismaSalvaguarda" placeholder="+0" type="text" />
+              <input name="carismaSalvaguardaProva" type="checkbox" />
+            </li>
+          </ul>
+          <div className="label">
+            Salvaguarda
+          </div>
         </div>
 
-        <div>
-          <label>
-            Inteligência
-            <input type="number"></input>
-          </label>
-          <fieldset>
-            <label>
-              Salvaguarda
-              <input type="checkbox"></input>
-            </label>
-            <label>
-              Arcanismo
-              <input type="checkbox"></input>
-            </label>
-            <label>
-              História
-              <input type="checkbox"></input>
-            </label>
-            <label>
-              Investigação
-              <input type="checkbox"></input>
-            </label>
-            <label>
-              Natureza
-              <input type="checkbox"></input>
-            </label>
-            <label>
-              Religião
-              <input type="checkbox"></input>
-            </label>
-          </fieldset>
+        <div className="habilidades secaoList box">
+          <ul>
+            <li>
+              <label for="acrobacia">Acrobacia <span class="habilidade">(Des)</span></label>
+              <input name="acrobacia" placeholder="+0" type="text" />
+              <input name="acrobacia-prova" type="checkbox" />
+            </li>
+            <li>
+              <label for="lidarAnimais">Lidar com Animais <span class="habilidade">(Sab)</span></label>
+              <input name="lidarAnimais" placeholder="+0" type="text" />
+              <input name="lidarAnimais-prova" type="checkbox" />
+            </li>
+            <li>
+              <label for="arcanismo">Arcanismo <span class="habilidade">(Int)</span></label>
+              <input name="arcanismo" placeholder="+0" type="text" />
+              <input name="arcanismo-prova" type="checkbox" />
+            </li>
+            <li>
+              <label for="atletismo">Atletismo <span class="skill">(For)</span></label>
+              <input name="atletismo" placeholder="+0" type="text" />
+              <input name="atletismo-prova" type="checkbox" />
+            </li>
+            <li>
+              <label for="enganacao">Enganação <span class="skill">(Car)</span></label>
+              <input name="enganacao" placeholder="+0" type="text" />
+              <input name="enganacao-prova" type="checkbox" />
+            </li>
+            <li>
+              <label for="historia">História <span class="skill">(Int)</span></label>
+              <input name="historia" placeholder="+0" type="text" />
+              <input name="historia-prova" type="checkbox" />
+            </li>
+            <li>
+              <label for="intuicao">Intuição <span class="skill">(Sab)</span></label>
+              <input name="intuicao" placeholder="+0" type="text" />
+              <input name="intuicao-prova" type="checkbox" />
+            </li>
+            <li>
+              <label for="intimidacao">Intimidação <span class="skill">(Car)</span></label>
+              <input name="intimidacao" placeholder="+0" type="text" />
+              <input name="intimidacao-prova" type="checkbox" />
+            </li>
+            <li>
+              <label for="investigacao">Investigação <span class="skill">(Int)</span></label>
+              <input name="investigacao" placeholder="+0" type="text" />
+              <input name="investigacao-prova" type="checkbox" />
+            </li>
+            <li>
+              <label for="medicina">Medicina <span class="skill">(Sab)</span></label>
+              <input name="medicina" placeholder="+0" type="text" />
+              <input name="medicina-prova" type="checkbox" />
+            </li>
+            <li>
+              <label for="natureza">Natureza <span class="skill">(Int)</span></label>
+              <input name="natureza" placeholder="+0" type="text" />
+              <input name="natureza-prova" type="checkbox" />
+            </li>
+            <li>
+              <label for="percepcao">Percepção <span class="skill">(Sab)</span></label>
+              <input name="percepcao" placeholder="+0" type="text" />
+              <input name="percepcao-prova" type="checkbox" />
+            </li>
+            <li>
+              <label for="atuacao">Atuação <span class="skill">(Car)</span></label>
+              <input name="atuacao" placeholder="+0" type="text" />
+              <input name="atuacao-prova" type="checkbox" />
+            </li>
+            <li>
+              <label for="persuasao">Persuasão <span class="skill">(Car)</span></label>
+              <input name="persuasao" placeholder="+0" type="text" />
+              <input name="persuasao-prova" type="checkbox" />
+            </li>
+            <li>
+              <label for="religiao">Religião <span class="skill">(Int)</span></label>
+              <input name="religiao" placeholder="+0" type="text" />
+              <input name="religiao-prova" type="checkbox" />
+            </li>
+            <li>
+              <label for="prestidigitacao">Prestidigitação <span class="skill">(Des)</span></label>
+              <input name="prestidigitacao" placeholder="+0" type="text" />
+              <input name="prestidigitacao-prova" type="checkbox" />
+            </li>
+            <li>
+              <label for="furtividade">Furtividade <span class="skill">(Des)</span></label>
+              <input name="furtividade" placeholder="+0" type="text" />
+              <input name="furtividade-prova" type="checkbox" />
+            </li>
+            <li>
+              <label for="sobrevivencia">Sobrevivência <span class="skill">(Sab)</span></label>
+              <input name="sobrevivencia" placeholder="+0" type="text" />
+              <input name="sobrevivencia-prova" type="checkbox" />
+            </li>
+          </ul>
+          <div className="label">
+            Habilidades
+          </div>
         </div>
+      </section>
 
-        <div>
-          <label>
-            Sabedoria
-            <input type="number"></input>
-          </label>
-          <fieldset>
-            <label>
-              Salvaguarda
-              <input type="checkbox"></input>
-            </label>
-            <label>
-              Intuição
-              <input type="checkbox"></input>
-            </label>
-            <label>
-              Lidar com animais
-              <input type="checkbox"></input>
-            </label>
-            <label>
-              Medicina
-              <input type="checkbox"></input>
-            </label>
-            <label>
-              Percepção
-              <input type="checkbox"></input>
-            </label>
-            <label>
-              Sobrevivência
-              <input type="checkbox"></input>
-            </label>
-          </fieldset>
+      <div className="sabedoriaPassiva box">
+        <div class="label-container">
+          <label for="percepcaoPassiva">Sabedoria Passiva (Percepção)</label>
         </div>
-
-        <div>
-          <label>
-            Carisma
-            <input type="number"></input>
-          </label>
-          <fieldset>
-            <label>
-              Salvaguarda
-              <input type="checkbox"></input>
-            </label>
-            <label>
-              Atuação
-              <input type="checkbox"></input>
-            </label>
-            <label>
-              Enganação
-              <input type="checkbox"></input>
-            </label>
-            <label>
-              Intimidação
-              <input type="checkbox"></input>
-            </label>
-            <label>
-              Persuasão
-              <input type="checkbox"></input>
-            </label>
-          </fieldset>
-        </div>
-
-        <label>
-          Sabedoria Passiva (Percepção)
-          <input type="text"></input>
-        </label>
-      </form>
-    </div>
+        <input name="percepcaoPassiva" placeholder="10" />
+      </div>
+    </section>
   )
-} 
+}
 
-function StatusMid(){
+function StatusMid() {
   return (
     <div className="status-mid">
       <p>status mid</p>
     </div>
   );
-} 
+}
 
-function StatusRight(){
+function StatusRight() {
   return (
     <div className="status-right">
       <p>status right</p>
     </div>
   );
-}  
+}
 
-function AttaksAndSummons(){
+function AttaksAndSummons() {
   return (
     <div className="attack">
       <form>
         {/*falta coisa aqui */}
         <label>
-        Ataques & Conjuração
+          Ataques & Conjuração
           <input type="text"></input>
         </label>
       </form>
     </div>
   )
-} 
+}
 
-function CharacteristcsAndTalents(){
+function CharacteristcsAndTalents() {
   return (
     <div className="carac">
       <form>
@@ -268,9 +316,9 @@ function CharacteristcsAndTalents(){
       </form>
     </div>
   )
-} 
+}
 
-function AnotherProeficiences(){
+function AnotherProeficiences() {
   return (
     <div className="outro">
       <form>
@@ -281,9 +329,9 @@ function AnotherProeficiences(){
       </form>
     </div>
   )
-} 
+}
 
-function EquipamentsAndNotes(){
+function EquipamentsAndNotes() {
   return (
     <div className="equip">
       <form>
@@ -294,6 +342,6 @@ function EquipamentsAndNotes(){
       </form>
     </div>
   )
-} 
+}
 
 export default App;
